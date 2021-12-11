defea420e8db5885c586ac1ae2df78601456874e
JankTI <1308315453@qq.com>
2021-12-11 23:28:49 +0800

    品牌展示

--- src/views/Home/components/home-category.vue
+++ src/views/Home/components/home-category.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="home-category">
+  <div class="home-category" @mouseleave="categoryId=null">
     <ul class="menu">
-      <li v-for="item in menuList" :key="item.id">
-        <RouterLink :to="`/category/${item.id}`">{{item.name}}</RouterLink>
+      <li
+        v-for="item in menuList"
+        :key="item.id"
+        :class="{active:categoryId&&categoryId===item.id}"
+        @mouseenter="categoryId = item.id"
+      >
+        <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
 
         <template v-if="item.children">
           <RouterLink
@@ -10,45 +15,94 @@
             :key="sub.id"
             :to="`/category/sub/${sub.id}`"
           >
-            {{sub.name}}
+            {{ sub.name }}
           </RouterLink>
         </template>
       </li>
     </ul>
+    <!-- 弹层 -->
+    <div class="layer">
+      <h4>{{currCategory && currCategory.id === 'brand' ? '品牌' : '分类'}}推荐 <small>根据您的购买或浏览记录推荐</small></h4>
+      <!-- 商品 -->
+      <ul v-if="currCategory && currCategory.goods">
+        <li v-for="item in currCategory.goods" :key="item.id">
+          <RouterLink to="/">
+            <img :src="item.picture" alt="" />
+            <div class="info">
+              <p class="name ellipsis-2">{{ item.name }}</p>
+              <p class="desc ellipsis">{{ item.desc }}</p>
+              <p class="price"><i>¥</i>{{ item.price }}</p>
+            </div>
+          </RouterLink>
+        </li>
+      </ul>
+
+      <!-- 品牌 -->
+      <ul v-if="currCategory && currCategory.brands">
+        <li class="brand" v-for="brand in currCategory.brands" :key="brand.id">
+          <RouterLink to="/">
+            <img :src="brand.picture" alt="" />
+            <div class="info">
+              <p class="place">
+                <i class="iconfont icon-dingwei"></i>{{ brand.place }}
+              </p>
+              <p class="name ellipsis">{{ brand.name }}</p>
+              <p class="desc ellipsis-2">{{ brand.desc }}</p>
+            </div>
+          </RouterLink>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import { computed,reactive } from "vue";
-import { useStore } from 'vuex';
+import { computed, reactive, ref } from "vue";
+import { useStore } from "vuex";
+import { findBrand } from '@/api/home'
 export default {
-  name: 'HomeCategory',
-  setup(){
-    const store = useStore()
-    // 最终使用的数据 = 9个分类 + 1个品牌
+  name: "HomeCategory",
+  setup() {
+    const store = useStore();
+    const brand = reactive({
+      id: "brand",
+      name: "品牌",
+      children: [{ id: "brand-children", name: "品牌推荐" }],
+      // 品牌列表
+      brands: [],
+    });
+    // 最终使用的数据 = 9个分类  1个品牌
     const menuList = computed(() => {
       // 得到9个分类 且 每个一级分类下的子分类只有两个
-      const brand = reactive({
-        id: 'brand',
-        name: '品牌',
-        children: [{ id: 'brand-children',name: '品牌推荐'}]
-      })
-      const list = store.state.category.list.map(item => {
+      const list = store.state.category.list.map((item) => {
         return {
           id: item.id,
           name: item.name,
           children: item.children && item.children.slice(0, 2),
-          goods: item.goods
-        }
-      })
-      list.push(brand)
-      return list
-    })
+          goods: item.goods,
+        };
+      });
+      list.push(brand);
+      return list;
+    });
+
+    // 得到弹层的推荐商品数据
+    const categoryId = ref(null);
+    const currCategory = computed(() => {
+      return menuList.value.find((item) => item.id === categoryId.value);
+    });
+
+    // 获取品牌数据 尽量不要使用async在setup上
+    findBrand().then(data => {
+      brand.brands = data.result
+    });
 
     return {
-      menuList
-    }
-  }
+      menuList,
+      categoryId,
+      currCategory,
+    };
+  },
 };
 </script>
 
@@ -56,7 +110,7 @@ export default {
 .home-category {
   width: 250px;
   height: 500px;
-  background: rgba(0,0,0,0.8);
+  background: rgba(0, 0, 0, 0.8);
   position: relative;
   z-index: 99;
   .menu {
@@ -64,7 +118,7 @@ export default {
       padding-left: 40px;
       height: 50px;
       line-height: 50px;
-      &:hover {
+      &:hover,&.active {
         background: @xtxColor;
       }
       a {
@@ -74,6 +128,99 @@ export default {
           font-size: 16px;
         }
       }
+    }
+  }
+  // 弹出层样式
+    .layer {
+    width: 990px;
+    height: 500px;
+    background: rgba(255,255,255,0.8);
+    position: absolute;
+    left: 250px;
+    top: 0;
+    display: none;
+    padding: 0 15px;
+    h4 {
+      font-size: 20px;
+      font-weight: normal;
+      line-height: 80px;
+      small {
+        font-size: 16px;
+        color: #666;
+      }
+    }
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+      li {
+        width: 310px;
+        height: 120px;
+        margin-right: 15px;
+        margin-bottom: 15px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        background: #fff;
+        &:nth-child(3n) {
+          margin-right: 0;
+        }
+        a {
+          display: flex;
+          width: 100%;
+          height: 100%;
+          align-items: center;
+          padding: 10px;
+          &:hover {
+            background: #e3f9f4;
+          }
+          img {
+              width: 95px;
+              height: 95px;
+          }
+          .info {
+            padding-left: 10px;
+            line-height: 24px;
+            width: 190px;
+            .name {
+              font-size: 16px;
+              color: #666;
+            }
+            .desc {
+              color: #999;
+            }
+            .price {
+              font-size: 22px;
+              color: @priceColor;
+              i {
+                font-size: 16px;
+              }
+            }
+          }
+        }
+      }
+      // 品牌的样式
+      li.brand {
+        height: 180px;
+        a {
+          align-items: flex-start;
+          img {
+            width: 120px;
+            height: 160px;
+          }
+          .info {
+            p {
+              margin-top: 8px;
+            }
+            .place {
+              color: #999;
+            }
+          }
+        }
+      }
+    }
+  }
+  &:hover {
+    .layer {
+      display: block;
     }
   }
 }
