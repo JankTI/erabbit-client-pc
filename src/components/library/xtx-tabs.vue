7d6a0f37fa4c070733f920f01cfe1b3dbfa72397
JankTI <1308315453@qq.com>
2021-12-26 16:23:52 +0800

    订单管理-tab组件

--- src/components/library/xtx-tabs.vue
+++ src/components/library/xtx-tabs.vue
@@ -1,23 +1,91 @@
 <script>
+import { useVModel } from "@vueuse/core";
+import { provide } from "vue";
 export default {
   name: "XtxTabs",
-  render() {
-    // 返回的内容会进行渲染
-    // 1. 在babel的帮助下 可以在vue中写jsx语法
-    // 2. 数据的使用
-    const name = "tom";
-    // 3. 事件如何绑定
-    const say = () => {
-      console.log("h1 jsx");
+  props: {
+    modelValue: {
+      type: [String, Number],
+      default: "",
+    },
+  },
+  setup(props, { emit }) {
+    // 接收v-model的值
+    const activeName = useVModel(props, "modelValue", emit);
+    // 点击选项卡触发函数
+    const tabClick = (name, index) => {
+      activeName.value = name;
+      // 提供一个tab-click自定义事件
+      emit("tab-click", { name, index });
     };
-    // 4. 定义一个jsx对象
-    const sub = <sub>123</sub>;
-    return (
-      <h1 title={name} onClick={say}>
-        {name}
-        {sub}
-      </h1>
+    // 给每一个panel传当前激活的name
+    provide("activeName", activeName);
+    return {
+      activeName,
+      tabClick,
+    };
+  },
+  render() {
+    // 获取插槽内容
+    const panels = this.$slots.default();
+    // 动态的panels组件集合
+    const dynamicPanels = [];
+    panels.forEach((item) => {
+      // 静态
+      if (item.type.name === "XtxTabsPanel") {
+        dynamicPanels.push(item);
+      } else {
+        // v-for 渲染出来的
+        item.children.forEach((item) => {
+          dynamicPanels.push(item);
+        });
+      }
+    });
+
+    // 需要在这里进行组织
+    // 1. tabs组件大容器
+    // 2. 选项卡的导航菜单结构
+    // 3. 内容容器
+    const nav = (
+      <nav>
+        {dynamicPanels.map((item, i) => {
+          return (
+            <a
+              onClick={() => this.tabClick(item.props.name, i)}
+              class={{ active: item.props.name === this.activeName }}
+              href="javascript:;"
+            >
+              {item.props.label}
+            </a>
+          );
+        })}
+      </nav>
     );
+
+    return <div class="xtx-tabs">{[nav, dynamicPanels]}</div>;
   },
 };
 </script>
+<style lang="less" scoped>
+.xtx-tabs {
+  background: #fff;
+  > nav {
+    height: 60px;
+    line-height: 60px;
+    display: flex;
+    border-bottom: 1px solid #f5f5f5;
+    > a {
+      width: 110px;
+      border-right: 1px solid #f5f5f5;
+      text-align: center;
+      font-size: 16px;
+      &.active {
+        border-top: 2px solid @xtxColor;
+        height: 60px;
+        background: #fff;
+        line-height: 56px;
+      }
+    }
+  }
+}
+</style>
--- src/views/Member/order/index.vue
+++ src/views/Member/order/index.vue
@@ -1,16 +1,36 @@
 <template>
   <div class="member-order">
-    MemberOrder
-    <!-- jsx语法 -->
-    <XtxTabs></XtxTabs>
+    <XtxTabs v-model="activeName">
+      <XtxTabsPanel
+        v-for="item in orderStatus"
+        :key="item.name"
+        :label="item.label"
+        :name="item.name"
+      >
+        {{ item.label }}
+      </XtxTabsPanel>
+    </XtxTabs>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
+import { orderStatus } from "@/api/constants";
 export default {
   name: "MemberOrder",
-  setup() {},
+  setup() {
+    const activeName = ref("all");
+    return {
+      activeName,
+      orderStatus,
+    };
+  },
 };
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.member-order {
+  height: 100%;
+  background: #fff;
+}
+</style>
